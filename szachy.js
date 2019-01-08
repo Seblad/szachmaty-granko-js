9bfbdb94bd7247a566e2d0a94cd1f674f0f1bf13
QuackCode <quack.code.dk@gmail.com>
2019-01-09 00:20:04 +0100

    szachy.js: simplified way to obtain alpha fields

--- szachy.js
+++ szachy.js
@@ -1,30 +1,24 @@
 // console.log("PRE: DOM loaded: ", document.body.innerHTML);
 
 document.addEventListener("DOMContentLoaded", function onDOMLoaded() {
-    // console.log("POST: DOM loaded: ", document.body.innerHTML)
     let chessboard = document.querySelector("#chessboard-automated");
-    // console.log("chessboard.htmlelement = ", chessboard);
-    // console.log("chessboard.htmlelement = ", document.querySelector("#chessboard-automated"));
-    // chessboard.innerText = "moje pole";
-
     /* for
     *  -> chessline (of chessboard)
     *      for
     *      -> field (of chessline) */
-    
-        for (let j=8 ;j>0; j--){
-            let chessline = document.createElement("div");
-            chessline.classList.add("chessline");
-            chessboard.appendChild(chessline);
-            chessline.id=String.fromCharCode(73 - j);
-            for (let i = 8; i >0 ; i--) {
-                let pole = document.createElement("div");
-                pole.classList.add("field");
-                chessline.appendChild(pole);
-                pole.id=String.fromCharCode(73 - j)+i;
-                console.log(`${i} pole: `, pole);
+    const ALPHA_FIELDS = "HGFEDCBA";
+    for (let j = 7; j >= 0; j--) {
+        let chessline = document.createElement("div");
+        chessline.classList.add("chessline");
+        chessboard.appendChild(chessline);
+        chessline.id = ALPHA_FIELDS[j];
+        for (let i = 8; i > 0; i--) {
+            let pole = document.createElement("div");
+            pole.classList.add("field");
+            chessline.appendChild(pole);
+            pole.id = ALPHA_FIELDS[j] + "" + i;
         }
-    
+
     }
     console.log("wzorzec: ", document.querySelector(".field"));
 });
